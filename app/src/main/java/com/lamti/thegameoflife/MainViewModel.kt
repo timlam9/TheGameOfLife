76417f94427e6a6b636b4fb40c3c47fc3c382ea2
TimLam92 <tilamprinos@gmail.com>
2022-08-28 13:45:13 +0300

    Move to background thread

--- app/src/main/java/com/lamti/thegameoflife/MainViewModel.kt
+++ app/src/main/java/com/lamti/thegameoflife/MainViewModel.kt
@@ -1,13 +1,16 @@
 package com.lamti.thegameoflife
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.lamti.thegameoflife.domain.GameBoard
 import com.lamti.thegameoflife.domain.GameEngine
 import com.lamti.thegameoflife.domain.Screen
 import com.lamti.thegameoflife.ui.theme.BOX_WIDTH
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 class MainViewModel(private val gameEngine: GameEngine = GameEngine()) : ViewModel() {
 
@@ -23,33 +26,47 @@ class MainViewModel(private val gameEngine: GameEngine = GameEngine()) : ViewMod
     val isMatrixThemeOn: MutableStateFlow<Boolean> = _isMatrixThemeOn
 
     fun initBoard(listRange: Int, columns: Int) {
-        gameEngine.createRandomBoard(listRange, columns)
+        viewModelScope.launch(Dispatchers.Default) {
+            gameEngine.createRandomBoard(listRange, columns)
+        }
     }
 
     fun updateState() {
-        gameEngine.updateState()
+        viewModelScope.launch(Dispatchers.Default) {
+            gameEngine.updateState()
+        }
     }
 
     fun restartGame(listRange: Int, columns: Int) {
-        gameEngine.createRandomBoard(listRange, columns)
+        viewModelScope.launch(Dispatchers.Default) {
+            gameEngine.createRandomBoard(listRange, columns)
+        }
     }
 
     fun settingsButtonClicked() {
-        _screenStateFlow.update { Screen.Settings }
+        viewModelScope.launch(Dispatchers.Default) {
+            _screenStateFlow.update { Screen.Settings }
+        }
     }
 
     fun closeSettingsButtonClicked() {
-        _screenStateFlow.update { Screen.Game }
+        viewModelScope.launch(Dispatchers.Default) {
+            _screenStateFlow.update { Screen.Game }
+        }
     }
 
     fun onSliderValueChanged(value: Float) {
-        _cellSizeFlow.update {
-            (value * 80).toInt()
+        viewModelScope.launch(Dispatchers.Default) {
+            _cellSizeFlow.update {
+                (value * 80).toInt()
+            }
         }
     }
 
     fun matrixThemeClicked() {
-        _isMatrixThemeOn.update { !it }
+        viewModelScope.launch(Dispatchers.Default) {
+            _isMatrixThemeOn.update { !it }
+        }
     }
 
 }
--- app/src/main/java/com/lamti/thegameoflife/domain/GameBox.kt
+++ app/src/main/java/com/lamti/thegameoflife/domain/GameBox.kt
@@ -3,18 +3,18 @@ package com.lamti.thegameoflife.domain
 data class GameBox(
     val status: LifeStatus = LifeStatus.Dead,
     val index: Int
-) {
+)
 
-    fun findNeighbours(columns: Int, listSize: Int): List<Int> {
-        val north = if (index - columns > 0) index - columns else 0
-        val south = if (index + columns < listSize) index + columns else 0
-        val east = if (index.rem(columns) == 0) 0 else index + 1
-        val west = if ((index - 1).rem(columns) == 0) 0 else index - 1
-        val northWest = if (north == 0) 0 else north - 1
-        val northEast = if (north == 0) 0 else north + 1
-        val southWest = if (south == 0) 0 else south - 1
-        val southEast = if (south == 0) 0 else south + 1
+fun GameBox.findNeighbours(columns: Int, listSize: Int): List<Int> {
+    val north = if (index - columns > 0) index - columns else 0
+    val south = if (index + columns < listSize) index + columns else 0
+    val east = if (index.rem(columns) == 0) 0 else index + 1
+    val west = if ((index - 1).rem(columns) == 0) 0 else index - 1
+    val northWest = if (north == 0) 0 else north - 1
+    val northEast = if (north == 0) 0 else north + 1
+    val southWest = if (south == 0) 0 else south - 1
+    val southEast = if (south == 0) 0 else south + 1
 
-        return listOf(north, south, east, west, northWest, northEast, southWest, southEast).filterNot { it == 0 }
-    }
+    return listOf(north, south, east, west, northWest, northEast, southWest, southEast).filterNot { it == 0 }
 }
+
